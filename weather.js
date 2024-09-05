08fbc02b8a4687d21a97c6ba0cdc3f3262a3b58f
iChen-Reymar <reymarobenza.202300232@gmail.com>
2024-09-06 00:07:18 +0800

    finish

--- weather.js
+++ weather.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     const searchButton = document.getElementById('searchButton');
     const cityInput = document.getElementById('cityInput');
